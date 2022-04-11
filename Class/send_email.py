e619f8c505a738a0abd721959f27d93cb522c406
Paula Letícia Gerónimo <paulagero182@gmail.com>
2022-04-10 19:02:44 -0500

    Update send_email.py

--- Class/send_email.py
+++ Class/send_email.py
@@ -19,9 +19,9 @@ except ImportError:
 def send_email(subject, body, files):
     import smtplib #SMTP Library
     from smtplib import SMTPException
-    from email.mine.base import MINEBase
-    from email.mine.multipart import MIMEMultipart
-    from email.mine.text import MIMEText
+    from email.mime.base import MIMEBase
+    from email.mime.multipart import MIMEMultipart
+    from email.mime.text import MIMEText
     from email import encoders
 
     sender_email = SENDER_EMAIL
@@ -45,7 +45,7 @@ def send_email(subject, body, files):
         attach_path = filename
         if attach_path.exists() and os.path.getsize(filename) < MAX_FILE_SIZE:
             with open(os.path.join(filename), 'rb') as attachment:
-                part = MINEBase('application', 'octet-stream')
+                part = MIMEBase('application', 'octet-stream')
                 part.set_payload(attachment.read())
                 encoders.encode_base64(part)
                 part.add_header(
@@ -54,6 +54,19 @@ def send_email(subject, body, files):
                 message.attach(part)
         else:
             logger.warning (f"Failed to attach {attach_path.as_posix()} as the path did not exist or the file was larger than {MAX_FILE_SIZE} bytes.")
+
+        text = message.as_string()
+        
+        try:
+            smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+            smtpobj.ehlo()
+            smtpobj.starttls()
+            smtpobj.ehlo()
+            smtpobj.login(sender_email, MY_PASSWORD)
+            smtpobj.sendmail(sender_email, to_emails, text)
+            smtpobj.close()
+        except SMTPException:
+            logger.exception(f"Unable to send email from {sender_email} to {','.join(to_emails)}.")
 
 
 
